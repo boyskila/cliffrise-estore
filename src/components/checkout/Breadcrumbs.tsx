6d7e47baf7ac4f91cf27e584325ae48e12a0a318
boiskila <boiskila@gmail.com>
2025-02-18 00:55:16 +0200

    change Breadcrumbs icons

--- src/components/checkout/Breadcrumbs.tsx
+++ src/components/checkout/Breadcrumbs.tsx
@@ -1,6 +1,8 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
 export const Breadcrumbs = () => {
   return (
-    <nav className="flex" aria-label="Breadcrumb">
+    <nav className="flex bg-transparent" aria-label="Breadcrumb">
       <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
         <li className="inline-flex items-center">
           <div className="inline-flex items-center text-sm font-medium text-gray-700 dark:text-gray-400">
@@ -9,21 +11,7 @@ export const Breadcrumbs = () => {
         </li>
         <li>
           <div className="flex items-center">
-            <svg
-              className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
+            <ChevronRightIcon className="h-3" />
             <div className="ms-1 text-sm font-medium text-gray-700 md:ms-2 dark:text-gray-400">
               Shipping
             </div>
@@ -31,21 +19,7 @@ export const Breadcrumbs = () => {
         </li>
         <li aria-current="page">
           <div className="flex items-center">
-            <svg
-              className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
+            <ChevronRightIcon className="h-3" />
             <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
               Payment
             </span>
